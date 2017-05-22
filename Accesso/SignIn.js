76de9624f9b0fc2371c67722de119950eb6cc44b
Mattia <mattiapoliti@gmail.com>
2017-05-22 16:29:49 +0200

    Pagina di Sign In e Log In

---
+++ Accesso/SignIn.js
@@ -0,0 +1,29 @@
+window.onload = function(e) {
+// Get the modal
+    var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+    var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+    var btn2 = document.getElementById("close");
+
+    console.log("Sto cazzo");
+
+// When the user clicks on the button, open the modal
+    btn.onclick = function () {
+        modal.style.display = "block";
+    }
+
+// When the user clicks on <span> (x), close the modal
+    btn2.onclick = function () {
+        modal.style.display = "none";
+    }
+
+/*When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }*/
+}
