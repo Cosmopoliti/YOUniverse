f96b24b6ee2cbfae68cefd6dcfb8fa6672f5faf6
bbogge <marco.oggero93@gmail.com>
2017-06-08 15:52:08 +0200

    some change

--- Profilo Utente/Cambio.js
+++ Profilo Utente/Cambio.js
@@ -1,7 +1,11 @@
 /**
  * Created by Mattia on 31/05/2017.
  */
+<<<<<<< Updated upstream
 var imgd=
+=======
+
+>>>>>>> Stashed changes
 
 angular.module("myProfilo", ['ngRoute'])
 .config(['$routeProvider', function ($routeProvider) {
@@ -10,6 +14,7 @@ angular.module("myProfilo", ['ngRoute'])
             templateUrl: 'sub-views/informazioni.html'
         })
         .when('/Interessi', {
+<<<<<<< Updated upstream
             templateUrl: 'sub-views/interessi.html'
         })
         .when('/Diario', {
@@ -25,4 +30,26 @@ angular.module("myProfilo", ['ngRoute'])
             templateUrl: 'sub-views/palmares.html'
         })
         .otherwise({redirectTo: 'sub-views/Informazioni'});
+=======
+            templateUrl: 'sub-views/interessi.html',
+            controller: 'Ctrl'
+        })
+        .when('/Diario', {
+            templateUrl: 'sub-views/diario.html',
+            controller: 'Ctrl'
+        })
+        .when('/Libreria', {
+            templateUrl: 'libreria.html',
+            controller: 'Ctrl'
+        })
+        .when('/Storie', {
+            templateUrl: 'storie.html',
+            controller: 'Ctrl'
+        })
+        .when('/Palmares', {
+            templateUrl: 'palmares.html',
+            controller: 'Ctrl'
+        })
+        .otherwise({redirectTo: '/Informazioni'});
+>>>>>>> Stashed changes
 }]);
