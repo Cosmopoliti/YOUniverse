5aaa019db0a7a3277217c3c4cb196b20bcb94203
bbogge <marco.oggero93@gmail.com>
2017-06-09 14:57:35 +0200

    Merge branch 'master' of https://github.com/SoNet-2017/YOUniverse-code
    # Conflicts:
    #	Profilo Utente/Cambio.js
    #	Profilo Utente/sub-views/diario.html

--- Profilo Utente/Cambio.js
+++ Profilo Utente/Cambio.js
@@ -1,11 +1,7 @@
 /**
  * Created by Mattia on 31/05/2017.
  */
-<<<<<<< Updated upstream
-var imgd=
-=======
 
->>>>>>> Stashed changes
 
 angular.module("myProfilo", ['ngRoute'])
 .config(['$routeProvider', function ($routeProvider) {
@@ -14,7 +10,6 @@ angular.module("myProfilo", ['ngRoute'])
             templateUrl: 'sub-views/informazioni.html'
         })
         .when('/Interessi', {
-<<<<<<< Updated upstream
             templateUrl: 'sub-views/interessi.html'
         })
         .when('/Diario', {
@@ -29,27 +24,5 @@ angular.module("myProfilo", ['ngRoute'])
         .when('/Palmares', {
             templateUrl: 'sub-views/palmares.html'
         })
-        .otherwise({redirectTo: 'sub-views/Informazioni'});
-=======
-            templateUrl: 'sub-views/interessi.html',
-            controller: 'Ctrl'
-        })
-        .when('/Diario', {
-            templateUrl: 'sub-views/diario.html',
-            controller: 'Ctrl'
-        })
-        .when('/Libreria', {
-            templateUrl: 'libreria.html',
-            controller: 'Ctrl'
-        })
-        .when('/Storie', {
-            templateUrl: 'storie.html',
-            controller: 'Ctrl'
-        })
-        .when('/Palmares', {
-            templateUrl: 'palmares.html',
-            controller: 'Ctrl'
-        })
         .otherwise({redirectTo: '/Informazioni'});
->>>>>>> Stashed changes
 }]);
